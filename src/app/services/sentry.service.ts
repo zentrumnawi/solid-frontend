aac3fc66364c8b1cc93095aa34fcbfc5320dd806
mkay-sh <code@mkay-sh.de>
2019-03-04 01:44:34 +0100

    Fixed build script and send sentry reports only in production envs.

--- src/app/services/sentry.service.ts
+++ src/app/services/sentry.service.ts
@@ -3,7 +3,7 @@ import {ErrorHandler, Injectable} from "@angular/core";
 import {environment} from "../../environments/environment";
 const version = require('../../environments/version.json');
 
-// if (environment.production) {
+if (environment.production) {
   Sentry.init({
     dsn: 'https://5485ad7838ed4a118308f88c5f4650e1@po-sentry.physikelearning.de/7',
     release: version.hash,
@@ -17,15 +17,15 @@ const version = require('../../environments/version.json');
       return event;
     }
   });
-// }
+}
 
 @Injectable()
 export class SentryErrorHandler implements ErrorHandler {
   constructor() {}
   handleError(error: any) {
-    // if (environment.production) {
+    if (environment.production) {
       Sentry.captureException(error.originalError || error);
-    // }
+    }
     throw error;
   }
 }
