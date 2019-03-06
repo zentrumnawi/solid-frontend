b8c9b4ffbe489674f74f2cb9ea653dbd740b3cc9
mkay-sh <code@mkay-sh.de>
2019-03-06 16:48:59 +0100

    Hide StepperHeader in determinationhelper. Navigate with arrow right and left

--- src/app/modules/determinationhelper/components/determination-helper/determination-helper.component.ts
+++ src/app/modules/determinationhelper/components/determination-helper/determination-helper.component.ts
@@ -1,4 +1,10 @@
-import {Component} from '@angular/core';
+import {Component, HostListener, ViewChild} from '@angular/core';
+import {MatStepper} from "@angular/material";
+
+export enum KEY {
+  RIGHT_ARROW = 'ArrowRight',
+  LEFT_ARROW = 'ArrowLeft'
+}
 
 @Component({
   selector: 'app-determination-helper',
@@ -7,6 +13,16 @@ import {Component} from '@angular/core';
 })
 export class DeterminationHelperComponent {
   public MaxStep = 0;
+  @ViewChild('stepper') public Stepper: MatStepper;
+
+  @HostListener('window:keyup', ['$event'])
+  public keyEvent(event: KeyboardEvent) {
+    if (event.key === KEY.LEFT_ARROW) {
+      this.Stepper.previous();
+    } else if (event.key === KEY.RIGHT_ARROW) {
+      this.Stepper.next();
+    }
+  }
 
   public onNextStepClick(stepId: number) {
     if (this.MaxStep < stepId) {
