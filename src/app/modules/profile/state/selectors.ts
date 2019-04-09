e6991cbc27d611795ffd55c4b89932a9de19ee98
mkay-sh <code@mkay-sh.de>
2019-04-09 13:29:16 +0200

    Updated selectors (removed deprecated createSelector calls)

--- src/app/modules/profile/state/selectors.ts
+++ src/app/modules/profile/state/selectors.ts
@@ -1,11 +1,8 @@
-import {createSelector} from '@ngrx/store';
 import {oc} from 'ts-optchain';
 import {MineralProfile, Profile, ProfileAppState, ProfileCategory} from './profile.model';
 import {GalleryAppState} from './gallery.model';
 
-export const selectProfiles = createSelector(
-  (state: ProfileAppState) => oc(state).profile([]),
-);
+export const selectProfiles = (state: ProfileAppState) => oc(state).profile([]);
 
 const findProfileDeep = (profiles: Profile[], profileId: number): { profile: MineralProfile, category: ProfileCategory | null } | null => {
   for (let profile of profiles) {
@@ -19,17 +16,15 @@ const findProfileDeep = (profiles: Profile[], profileId: number): { profile: Min
   return null;
 };
 
-export const selectProfile = createSelector((state: ProfileAppState, profileId: number) => {
+export const selectProfile = (state: ProfileAppState, profileId: number) => {
   for (let profile of state.profile) {
     if (profile.type === 'category') {
       const childSearch = findProfileDeep(profile.children, profileId);
       if (childSearch) {
-        return { profile: childSearch.profile, category: childSearch.category ? childSearch.category : profile };
+        return {profile: childSearch.profile, category: childSearch.category ? childSearch.category : profile};
       }
     }
   }
-});
+};
 
-export const selectPhotographs = createSelector(
-  (state: GalleryAppState) => oc(state).gallery([]),
-);
+export const selectPhotographs = (state: GalleryAppState) => oc(state).gallery([]);
