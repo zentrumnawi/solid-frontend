b4ecc14546a1e244be19d8658da85fc5ae7fa1d9
mkay-sh <code@mkay-sh.de>
2019-05-03 11:08:08 +0200

    Added initial markdown renderer

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,24 +1,29 @@
-import {LayoutModule} from '@angular/cdk/layout';
-import {STEPPER_GLOBAL_OPTIONS} from '@angular/cdk/stepper';
-import {CommonModule} from '@angular/common';
-import {HttpClientModule} from '@angular/common/http';
-import {ModuleWithProviders, NgModule} from '@angular/core';
-import {MatButtonModule} from '@angular/material/button';
-import {MatCardModule} from '@angular/material/card';
-import {MatDialogModule} from '@angular/material/dialog';
-import {MatIconModule} from '@angular/material/icon';
-import {MatInputModule} from "@angular/material/input";
-import {MatListModule} from '@angular/material/list';
-import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
-import {MatSelectModule} from "@angular/material/select";
-import {MatSidenavModule} from '@angular/material/sidenav';
-import {MatStepperModule} from '@angular/material/stepper';
-import {MatToolbarModule} from '@angular/material/toolbar';
-import {ImageCardComponent} from './components/image-card/image-card.component';
-import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import { MarkdownService } from './services/markdown.service';
+import { LayoutModule } from '@angular/cdk/layout';
+import { STEPPER_GLOBAL_OPTIONS } from '@angular/cdk/stepper';
+import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
+import { ModuleWithProviders, NgModule } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatListModule } from '@angular/material/list';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatSelectModule } from '@angular/material/select';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatStepperModule } from '@angular/material/stepper';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { ImageCardComponent } from './components/image-card/image-card.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MarkdownComponent } from './components/markdown/markdown.component';
 
 @NgModule({
-  declarations: [ImageCardComponent],
+  declarations: [
+    ImageCardComponent,
+    MarkdownComponent,
+  ],
   imports: [
     CommonModule,
     FormsModule,
@@ -55,6 +60,10 @@ import {FormsModule, ReactiveFormsModule} from "@angular/forms";
     MatToolbarModule,
     LayoutModule,
     ImageCardComponent,
+    MarkdownComponent,
+  ],
+  providers: [
+    MarkdownService,
   ]
 })
 export class SharedModule {
@@ -64,7 +73,7 @@ export class SharedModule {
       providers: [
         {
           provide: STEPPER_GLOBAL_OPTIONS,
-          useValue: {displayDefaultIndicatorType: false},
+          useValue: { displayDefaultIndicatorType: false },
         },
       ],
     };
