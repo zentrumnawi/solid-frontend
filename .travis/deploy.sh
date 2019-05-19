17c3ae202ff425ca975387a3beb7425389c28075
Michael Gecht <gecht.m@gmail.com>
2019-05-19 19:17:59 +0200

    Only deploy on correct git branches

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -5,7 +5,7 @@ TRAVIS_PR=${1}
 TRAVIS_BRANCH=${2}
 
 # Exit if this is a pull request
-if [[ ${TRAVIS_PR} != "false" ]]; then
+if [[ ${TRAVIS_PR} != "false" ]] || [[ ${TRAVIS_BRANCH} != "dev" && ${TRAVIS_BRANCH} != "master" && ${TRAVIS_BRANCH} != "preview" ]]; then
   echo "We do not deploy pull requests."
   exit 1
 fi
