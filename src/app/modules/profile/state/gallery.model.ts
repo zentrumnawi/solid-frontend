7a84f0f737a1d879e06821f3b5e95616c0906465
mkay-sh <code@mkay-sh.de>
2019-06-04 10:45:05 +0200

    Updated gallery model

--- src/app/modules/profile/state/gallery.model.ts
+++ src/app/modules/profile/state/gallery.model.ts
@@ -13,26 +13,14 @@ export interface PhotographModel {
   id: number;
   image_file: ImageFiles;
   handpiece: HandpieceModel;
-  orientation: PhotographOrientation;
-  shot_type: ShotType;
+  orig_height: number;
+  orig_width: number;
+  audio_file: string | null;
+  description: string | null;
   online_status: boolean;
   created_at: Date;
   last_modified: Date;
 }
-
-export enum PhotographOrientation {
-  T = 'T',
-  B = 'B',
-  S = 'S',
-}
-
-export enum ShotType {
-  MI = 'MI',
-  MA = 'MA',
-  FE = 'FE',
-  TL = 'TL'
-}
-
 
 export interface HandpieceModel {
   id: number;
