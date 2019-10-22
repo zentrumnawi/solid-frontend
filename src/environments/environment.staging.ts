cf57f267c1cc3779208e4fa33e6d740b880e2108
mkay-sh <code@mkay-sh.de>
2019-10-22 10:52:22 +0200

    Fixex QuizUrl

--- src/environments/environment.staging.ts
+++ src/environments/environment.staging.ts
@@ -2,6 +2,6 @@ export const environment = {
   preview: true,
   production: true,
   apiUrl: 'https://geomat-staging.physikelearning.de',
-  apiUrlQuiz: 'https://api.geomat.uni-frankfurt.de',
+  apiUrlQuiz: 'https://geomat.uni-frankfurt.de',
   sentryUrl: 'https://bd5075aa5fa4469399f1ba68e6413a3a@po-sentry.physikelearning.de/19',
 };
