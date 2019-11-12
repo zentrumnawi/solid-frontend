b30fd21d93035f948fdf496b5eb36f33bcadfd22
mkay-sh <code@mkay-sh.de>
2019-11-12 11:35:18 +0100

    Browsercheck #316

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,34 @@
 import 'hammerjs';
 
-import { enableProdMode } from '@angular/core';
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import {enableProdMode} from '@angular/core';
+import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
 
-import { AppModule } from './app/app.module';
-import { environment } from './environments/environment';
+import {AppModule} from './app/app.module';
+import {environment} from './environments/environment';
 
-if (environment.production) {
-  enableProdMode();
+import {detect} from 'detect-browser';
+
+const browser = detect();
+
+let supportedBrowser = true;
+
+if (browser) {
+  switch (browser.name) {
+    case 'ie':
+      const text = document.createTextNode(`Ihr Webbrowser (Internet Explorer ${browser.version}) ist veraltet und wird nicht von der Anwendung unterstützt. Bitte nutzen Sie einen neueren Browser.`);
+      const body = document.getElementsByTagName('body');
+      body[0].appendChild(text);
+      supportedBrowser = false;
+      break;
+    default:
+      break;
+  }
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+if (supportedBrowser) {
+  if (environment.production) {
+    enableProdMode();
+  }
+  platformBrowserDynamic().bootstrapModule(AppModule)
+    .catch(err => console.error(err));
+}
