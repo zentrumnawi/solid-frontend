a7310abd613501312811906e74a06fc0a3ef6ab6
mkay-sh <code@mkay-sh.de>
2020-02-13 14:28:32 +0100

    #360

--- src/app/modules/crystalsystem/components/crystalsystemdetail/crystalsystemdetail.component.ts
+++ src/app/modules/crystalsystem/components/crystalsystemdetail/crystalsystemdetail.component.ts
@@ -5,49 +5,83 @@ import {configurations} from "./crystalsystemdetail-configuration";
 
 type Models = 'cubic' | 'hexagonal' | 'monoclinic' | 'orthorhombic' | 'trigonal' | 'tetragonal' | 'triclinic';
 
+interface CrystalsystemIFrameWindowProxy {
+  toggleFaces?: () => void;
+  togglePoints?: () => void;
+  togglePerspective?: () => void;
+  toggleAxis?: () => void;
+  toggleHighlight?: (layerNumber: number) => void;
+  switchModel?: (modelName: string) => void;
+}
+
 @Component({
   selector: 'app-crystalsystemdetail',
   templateUrl: './crystalsystemdetail.component.html',
   styleUrls: ['./crystalsystemdetail.component.scss']
 })
 export class CrystalsystemdetailComponent {
-  @ViewChild('iframe', {static: false}) public IFrame!: ElementRef;
+  @ViewChild('iframe', {static: false}) public iFrame: ElementRef<HTMLIFrameElement> | null = null;
   public Configs = configurations;
   public SelectedConfig = configurations[0];
-  public Layer = configurations[0].layers.length > 0 ? configurations[0].layers[0].name : '';
+  public Layer = configurations[0].layers.length > 0 ? configurations[0].layers[0].name : 0;
   public ShowInPreview = environment.preview;
   public Model = this.SelectedConfig.name;
+
+  public get contentWindow(): CrystalsystemIFrameWindowProxy | null {
+    if (this.iFrame && this.iFrame.nativeElement.contentWindow) {
+      this.iFrame.nativeElement.contentWindow;
+    }
+    return null;
+  }
 
   constructor(private _dialog: MatDialog) {
   }
 
   public onToggleSolidClick() {
-    this.IFrame.nativeElement.contentWindow.toggleFaces();
+    const cw = this.contentWindow;
+    if (cw && cw.toggleFaces) {
+      cw.toggleFaces();
+    }
   }
 
   public onTogglePointsClick() {
-    this.IFrame.nativeElement.contentWindow.togglePoints();
+    const cw = this.contentWindow;
+    if (cw && cw.togglePoints) {
+      cw.togglePoints();
+    }
   }
 
   public onTogglePerspectiveClick() {
-    this.IFrame.nativeElement.contentWindow.togglePerspective();
+    const cw = this.contentWindow;
+    if (cw && cw.togglePerspective) {
+      cw.togglePerspective();
+    }
   }
 
   public onToggleAxisClick() {
-    this.IFrame.nativeElement.contentWindow.toggleAxis();
+    const cw = this.contentWindow;
+    if (cw && cw.toggleAxis) {
+      cw.toggleAxis();
+    }
   }
 
   public onLayerSelectChange(newLayer: number) {
-    this.Layer = newLayer;
-    this.IFrame.nativeElement.contentWindow.toggleHighlight(newLayer);
+    const cw = this.contentWindow;
+    if (cw && cw.toggleHighlight) {
+      this.Layer = newLayer;
+      cw.toggleHighlight(newLayer);
+    }
   }
 
   public onModelSelectChange(newModel: Models) {
-    this.SelectedConfig = this.Configs.find(c => c.name === newModel)!;
-    this.Model = this.SelectedConfig.name;
-    this.Layer = this.SelectedConfig.layers.length > 0 ? this.SelectedConfig.layers[0].name : '';
-    this.IFrame.nativeElement.contentWindow.switchModel(newModel);
-    this.IFrame.nativeElement.contentWindow.toggleHighlight(this.Layer);
+    const cw = this.contentWindow;
+    if (cw && cw.switchModel && cw.toggleHighlight) {
+      this.SelectedConfig = this.Configs.find(c => c.name === newModel)!;
+      this.Model = this.SelectedConfig.name;
+      this.Layer = this.SelectedConfig.layers.length > 0 ? this.SelectedConfig.layers[0].name : 0;
+      cw.switchModel(newModel);
+      cw.toggleHighlight(this.Layer);
+    }
   }
 
   onInfoClick() {
