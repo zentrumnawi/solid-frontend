3a38fa1115e5f4818fb1bd5d5f13a22ed1d51d73
mkay-sh <code@mkay-sh.de>
2020-03-27 08:19:38 +0100

    better travis

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -7,7 +7,7 @@ TRAVIS_BRANCH=${2}
 # Exit if this is a pull request
 if [[ ${TRAVIS_PR} != "false" ]] || [[ ${TRAVIS_BRANCH} != "dev" && ${TRAVIS_BRANCH} != "master" && ${TRAVIS_BRANCH} != "preview" ]]; then
   echo "We do not deploy pull requests."
-  exit 1
+  exit 0
 fi
 
 # Perform the actual deploy
