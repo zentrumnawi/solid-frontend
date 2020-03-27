5df84b68f084a0f64ce4c0a2cb671720bd0e35f8
mkay-sh <code@mkay-sh.de>
2020-03-27 08:12:16 +0100

    fixed build

--- apps/geomat/src/environments/environment.staging.ts
+++ apps/geomat/src/environments/environment.staging.ts
@@ -2,6 +2,6 @@ export const environment = {
   preview: true,
   production: true,
   apiUrl: 'https://geomat-staging.physikelearning.de',
-  sentryUrl:
+  sentry:
     'https://bd5075aa5fa4469399f1ba68e6413a3a@po-sentry.physikelearning.de/19'
 };
