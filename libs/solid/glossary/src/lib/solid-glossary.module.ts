6347c9204a58ccf548b893f9f023b1658aef4e25
mkay-sh <code@mkay-sh.de>
2020-07-09 11:04:59 +0200

    Filter for glossary Fixes #549

--- libs/solid/glossary/src/lib/solid-glossary.module.ts
+++ libs/solid/glossary/src/lib/solid-glossary.module.ts
@@ -5,6 +5,9 @@ import { MatListModule } from '@angular/material/list';
 import { NgxsModule } from '@ngxs/store';
 import { GlossaryState } from './glossary.state';
 import { RefDirective } from './components/link.directive';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 
 // This workaround is required for the "old" angular compiler in production mode. Ivy library publishing is not supported until angular 10.
 // https://github.com/ng-packagr/ng-packagr/issues/767
@@ -12,7 +15,14 @@ export const ngxsFeatureModule = NgxsModule.forFeature([GlossaryState]);
 
 @NgModule({
   declarations: [GlossaryComponent, RefDirective],
-  imports: [SolidCoreModule, MatListModule, ngxsFeatureModule],
+  imports: [
+    SolidCoreModule,
+    MatListModule,
+    ngxsFeatureModule,
+    MatInputModule,
+    MatIconModule,
+    MatButtonModule,
+  ],
   exports: [GlossaryComponent],
 })
 export class SolidGlossaryModule {}
