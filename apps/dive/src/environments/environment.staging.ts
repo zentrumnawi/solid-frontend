516ae1f7fb9dc703d04094de274593958ad4db5d
mkay-sh <code@mkay-sh.de>
2020-10-12 08:23:26 +0200

    Changed backend urls

--- apps/dive/src/environments/environment.staging.ts
+++ apps/dive/src/environments/environment.staging.ts
@@ -3,7 +3,7 @@ import * as version from './version.json';
 export const environment = {
   preview: true,
   production: true,
-  apiUrl: 'https://solid-backend.uni-frankfurt.de/dive/staging/',
+  apiUrl: 'https://solid-backend.uni-frankfurt.de/dive/staging',
   sentry: {
     dsn:
       'https://6772dbe6a4bb42e88711f20924eefa8e@po-sentry.physikelearning.de/34',
