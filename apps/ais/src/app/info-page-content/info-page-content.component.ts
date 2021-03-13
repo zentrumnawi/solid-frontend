bc167bdb1cedab199944197de8ca97b40cdad45c
Bertram Bühner <github@sheepyhollow.de>
2021-03-13 17:12:41 +0100

    modify & fix AIS info page

--- apps/ais/src/app/info-page-content/info-page-content.component.ts
+++ apps/ais/src/app/info-page-content/info-page-content.component.ts
@@ -1,12 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import * as version from '../../environments/version.json';
+import { Store } from '@ngxs/store';
+import {
+  FeedbackService,
+  SOLID_SKELETON_FEEDBACK_SERVICE,
+} from '@zentrumnawi/solid-skeleton';
 
 @Component({
   selector: 'ais-info-page-content',
   templateUrl: './info-page-content.component.html',
   styleUrls: ['./info-page-content.component.scss'],
 })
-export class InfoPageContentComponent implements OnInit {
-  constructor() {}
+export class InfoPageContentComponent {
+  public Version =
+    version && version.semver && version.semver.version
+      ? version.semver.version
+      : 'Version unbekannt';
 
-  ngOnInit(): void {}
+  constructor(
+    private _store: Store,
+    @Inject(SOLID_SKELETON_FEEDBACK_SERVICE) public feedback: FeedbackService
+  ) {}
 }
