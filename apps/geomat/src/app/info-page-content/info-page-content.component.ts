f073749cc27d755d23d7cb5fe9601a4ef824bda4
mkay <code@mkay-sh.de>
2021-03-18 10:13:52 +0100

    Removed unnecessary tests and removed store dependency from info-page-content.components

--- apps/geomat/src/app/info-page-content/info-page-content.component.ts
+++ apps/geomat/src/app/info-page-content/info-page-content.component.ts
@@ -1,6 +1,5 @@
 import { Component, Inject } from '@angular/core';
 import * as version from '../../environments/version.json';
-import { Store } from '@ngxs/store';
 import {
   FeedbackService,
   SOLID_SKELETON_FEEDBACK_SERVICE,
@@ -18,7 +17,6 @@ export class InfoPageContentComponent {
       : 'Version unbekannt';
 
   constructor(
-    private _store: Store,
     @Inject(SOLID_SKELETON_FEEDBACK_SERVICE) public feedback: FeedbackService
   ) {}
 }
