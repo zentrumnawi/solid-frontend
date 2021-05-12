5ce25f943183b52231680a402b441f6203d697ad
Bertram Bühner <github@sheepyhollow.de>
2021-05-12 23:09:32 +0200

    implement media-error-dialog component

---
+++ libs/solid/core/src/lib/components/media-error-dialog/media-error-dialog.component.ts
@@ -0,0 +1,14 @@
+import {Component, Inject} from '@angular/core';
+import {MAT_DIALOG_DATA} from "@angular/material/dialog";
+
+@Component({
+  selector: 'app-media-error-dialog',
+  templateUrl: './media-error-dialog.component.html',
+  styleUrls: ['./media-error-dialog.component.scss']
+})
+export class MediaErrorDialogComponent {
+  constructor(
+    @Inject(MAT_DIALOG_DATA) public Data: { title: string, content: string },
+  ) {  }
+
+}
