0d0f689a4d4e793e2e7d550b7da4afba4a5bfe27
anhnguyen-98 <71195698+anhnguyen-98@users.noreply.github.com>
2021-09-01 13:49:08 +0200

    shorten audio titel, change heigh of audio player

--- libs/solid/core/src/lib/components/audio-icon/audio-icon.component.ts
+++ libs/solid/core/src/lib/components/audio-icon/audio-icon.component.ts
@@ -1,10 +1,26 @@
-import { Component, Input } from '@angular/core';
+import {
+  Component,
+  Input,
+  OnChanges,
+  OnInit,
+  SimpleChanges,
+} from '@angular/core';
 
 @Component({
   selector: 'solid-core-audio-icon',
   templateUrl: './audio-icon.component.svg',
   styleUrls: ['./audio-icon.component.scss'],
 })
-export class AudioIconComponent {
+export class AudioIconComponent implements OnChanges {
   @Input() title!: string;
+
+  ngOnChanges(): void {
+    this.shortenTitle();
+  }
+
+  shortenTitle() {
+    if (this.title.length > 15) {
+      this.title = this.title.slice(0, 18) + '...';
+    }
+  }
 }
