ff119182d297670814128cee514f2a545256b6ef
anhnguyen-98 <71195698+anhnguyen-98@users.noreply.github.com>
2021-10-21 15:36:32 +0200

    restructure AIS Menu fixed #1072

--- apps/ais/src/app/solid-skeleton-config.ts
+++ apps/ais/src/app/solid-skeleton-config.ts
@@ -10,16 +10,20 @@ export const skeletonConfig: SolidSkeletonConfig = {
     landing: {
       svgIcon: 'icon',
       name: 'landing',
+      order: 0,
     },
     profile: {
       svgIcon: 'profile',
-      url: 'steckbriefe',
+      title: 'Fundstücke',
+      url: 'fundstuecke',
       name: 'profile',
+      order: 3,
     },
     quiz: {
       svgIcon: 'quiz',
       url: 'selbsttest',
       name: 'quiz',
+      order: 2,
     },
     privacy: {
       component: PrivacyComponent,
@@ -32,6 +36,7 @@ export const skeletonConfig: SolidSkeletonConfig = {
       title: 'Keramikwissen',
       url: 'keramikwissen',
       name: 'slideshow',
+      order: 1,
     },
     info: {
       svgIcon: 'info',
