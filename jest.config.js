27a15bc42b741db57e20b5a456c158150d122697
mkay <code@mkay-sh.de>
2021-11-18 09:30:26 +0100

    Migration update-jest-config-to-use-util

--- jest.config.js
+++ jest.config.js
@@ -1,15 +1,3 @@
-module.exports = {
-  projects: [
-    '<rootDir>/apps/geomat',
-    '<rootDir>/libs/solid/core',
-    '<rootDir>/libs/solid/quiz',
-    '<rootDir>/libs/solid/slideshow',
-    '<rootDir>/libs/solid/glossary',
-    '<rootDir>/libs/solid/skeleton',
-    '<rootDir>/libs/solid/profile',
-    '<rootDir>/apps/dive',
-    '<rootDir>/apps/ais',
-    '<rootDir>/apps/planty',
-    '<rootDir>/libs/tools',
-  ],
-};
+const { getJestProjects } = require('@nrwl/jest');
+
+module.exports = { projects: [...getJestProjects(), '<rootDir>/libs/tools'] };
