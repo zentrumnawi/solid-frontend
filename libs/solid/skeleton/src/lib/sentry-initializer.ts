dda4a3cb033d01407391eccd7086d955b9194b22
Bertram Bühner <sheepyhollow@users.noreply.github.com>
2021-12-28 11:46:53 +0100

    workaround version in sentry initializer
    It's hacky, but we've got to move on with the staging environment.

--- libs/solid/skeleton/src/lib/sentry-initializer.ts
+++ libs/solid/skeleton/src/lib/sentry-initializer.ts
@@ -6,7 +6,8 @@ export const initSentry = (cfg?: SentryConfig) => {
     enabled: cfg !== undefined,
     dsn: cfg?.dsn,
     environment: cfg?.environment,
-    release: cfg?.version.default.semver.version,
+    //release: cfg?.version.default.semver.version,
+    release: "2.4.0"
     integrations: [
       // new Integrations.BrowserTracing({
       //   tracingOrigins: ["localhost", "https://yourserver.io/api"],
