a41900b0bb6727d168e397b35990b04cb8fcc3f9
Bertram Bühner <sheepyhollow@users.noreply.github.com>
2021-12-28 12:17:14 +0100

    fix more typos
    change double to single quotes

--- libs/solid/skeleton/src/lib/sentry-initializer.ts
+++ libs/solid/skeleton/src/lib/sentry-initializer.ts
@@ -7,7 +7,7 @@ export const initSentry = (cfg?: SentryConfig) => {
     dsn: cfg?.dsn,
     environment: cfg?.environment,
     //release: cfg?.version.default.semver.version,
-    release: "2.4.0",
+    release: '2.4.0',
     integrations: [
       // new Integrations.BrowserTracing({
       //   tracingOrigins: ["localhost", "https://yourserver.io/api"],
