daf770c05b8fac86c0a1f28bb3179aa4f7dce7b6
Bertram Bühner <github@sheepyhollow.de>
2022-03-17 14:18:08 +0100

    remove double version handling

--- apps/ais/src/app/info-page-content/info-page-content.component.ts
+++ apps/ais/src/app/info-page-content/info-page-content.component.ts
@@ -1,6 +1,5 @@
 import { Component, Inject, OnInit } from '@angular/core';
 import { version } from '../../environments/version';
-import pjs from '../../../../../package.json';
 
 import {
   FeedbackService,
@@ -16,8 +15,7 @@ export class InfoPageContentComponent {
   public appVersion =
     version && version.semver && version.semver.version
       ? version.semver.version
-      : undefined;
-  public solidVersion = pjs.version ? pjs.version : 'Version unbekannt';
+      : 'Version unbekannt';
 
   constructor(
     @Inject(SOLID_SKELETON_FEEDBACK_SERVICE) public feedback: FeedbackService
