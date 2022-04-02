8f68f9f81b0b8de3eee507d12345f48396270177
anhnguyen-98 <71195698+anhnguyen-98@users.noreply.github.com>
2022-04-02 12:30:36 +0200

    new slideshow structure, add base component

--- libs/solid/slideshow/src/lib/solid-slideshow-routing.module.ts
+++ libs/solid/slideshow/src/lib/solid-slideshow-routing.module.ts
@@ -1,20 +1,19 @@
 import { RouterModule, Routes } from '@angular/router';
-import { CategoriesSelectComponent } from './components/categories-select/categories-select.component';
-import { SlideshowSelectComponent } from './components/slideshow-select/slideshow-select.component';
-import { SlideshowComponent } from './components/slideshow/slideshow.component';
+import { SlideshowBaseComponent } from './components/slideshow-base/slideshow-base.component';
 
 const routes: Routes = [
   {
     path: '',
-    component: CategoriesSelectComponent,
-  },
-  {
-    path: ':categoriesSlug',
-    component: SlideshowSelectComponent,
-  },
-  {
-    path: ':categoriesSlug/:slideshowId',
-    component: SlideshowComponent,
+    component: SlideshowBaseComponent,
+    children: [
+      {
+        path: ':categoriesSlug/:slideshowId/:slideshowPageId',
+      },
+      {
+        path: '**',
+        redirectTo: '',
+      },
+    ],
   },
 ];
 
