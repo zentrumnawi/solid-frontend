0b43d48c4cb8fafc62e11deeac2a2528a3ee2d1c
anhnguyen-98 <71195698+anhnguyen-98@users.noreply.github.com>
2022-04-02 10:56:18 +0200

    add categories state to slideshow module

---
+++ libs/solid/slideshow/src/lib/state/categories.actions.ts
@@ -0,0 +1,3 @@
+export class GetCategories {
+  static readonly type = '[Category] get slideshow categories;';
+}
---
+++ libs/solid/slideshow/src/lib/state/categories.state.ts
@@ -0,0 +1,56 @@
+import { HttpClient } from '@angular/common/http';
+import { Inject, Injectable } from '@angular/core';
+import { Action, Selector, State, StateContext } from '@ngxs/store';
+import { SolidCoreConfig, SOLID_CORE_CONFIG } from '@zentrumnawi/solid-core';
+import { GetCategories } from './categories.actions';
+import { SlideshowCategory } from './categories.model';
+import { map, tap } from 'rxjs/operators';
+
+export type CategoriesStateModel = SlideshowCategory[];
+
+@State<CategoriesStateModel>({
+  name: 'categories',
+  defaults: [],
+})
+@Injectable()
+export class CategoriesState {
+  constructor(
+    private _http: HttpClient,
+    @Inject(SOLID_CORE_CONFIG) private _config: SolidCoreConfig
+  ) {}
+
+  @Selector()
+  public static getSlideshowCategoriesItems(state: CategoriesStateModel) {
+    const fn = () =>
+      state.map((s) => ({
+        id: s.id,
+        name: s.name,
+        slug: s.slug,
+      }));
+    return fn();
+  }
+
+  @Action(GetCategories)
+  public GetSlideshowCategories(ctx: StateContext<CategoriesStateModel>) {
+    if (ctx.getState().length > 0) {
+      return;
+    }
+    return this._http
+      .get<SlideshowCategory[]>(`${this._config.apiUrl}/categories`)
+      .pipe(
+        map((res) => {
+          const mapit = (input: SlideshowCategory[]): SlideshowCategory[] => {
+            return input.map((categories) => {
+              return {
+                ...categories,
+              };
+            });
+          };
+          return mapit(res);
+        }),
+        tap((res) => {
+          ctx.setState([...res.map((categories) => categories)]);
+        })
+      );
+  }
+}
