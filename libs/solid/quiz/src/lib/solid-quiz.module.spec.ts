74980079705d05d9409b847ea3cc5202f26c9c4f
Bertram Bühner <github@sheepyhollow.de>
2022-04-19 13:11:48 +0200

    fix format errors

--- libs/solid/quiz/src/lib/solid-quiz.module.spec.ts
+++ libs/solid/quiz/src/lib/solid-quiz.module.spec.ts
@@ -2,13 +2,11 @@ import { waitForAsync, TestBed } from '@angular/core/testing';
 import { SolidQuizModule } from './solid-quiz.module';
 
 describe('SolidQuizModule', () => {
-  beforeEach(
-    waitForAsync(() => {
-      TestBed.configureTestingModule({
-        imports: [SolidQuizModule],
-      }).compileComponents();
-    })
-  );
+  beforeEach(waitForAsync(() => {
+    TestBed.configureTestingModule({
+      imports: [SolidQuizModule],
+    }).compileComponents();
+  }));
 
   it('should create', () => {
     expect(SolidQuizModule).toBeDefined();
