3224676308421e47530c9e413ef84625c4a213e8
jdrandomperson <33594579+lthuong@users.noreply.github.com>
2022-05-14 22:21:46 +0200

    Single Choice Question component

---
+++ libs/solid/quiz/src/lib/components/single-choice-question/single-choice-question.component.ts
@@ -0,0 +1,36 @@
+import { Component, Input } from '@angular/core';
+import { MatRadioChange } from '@angular/material/radio';
+import { QuizAnswer, QuizQuestion } from '../../state/quiz.model';
+
+@Component({
+  selector: 'solid-quiz-single-choice-question',
+  templateUrl: './single-choice-question.component.html',
+  styleUrls: ['./single-choice-question.component.scss'],
+})
+export class SingleChoiceQuestionComponent {
+  @Input() public question!: QuizQuestion;
+  @Input() public SelectedAnswers!: number[];
+  @Input() public ShowAnswers!: boolean;
+
+  public onRadioChange(e: MatRadioChange) {
+    this.SelectedAnswers = [e.value];
+  }
+
+  public trackByFn(index: number, item: QuizAnswer) {
+    return item.id;
+  }
+
+  isAnswerCorrect(answer: QuizAnswer) {
+    if (!this.ShowAnswers) {
+      return false;
+    }
+    return answer.correct; // && this.SelectedAnswers.includes(answer.id);
+  }
+
+  isAnswerIncorrect(answer: QuizAnswer) {
+    if (!this.ShowAnswers) {
+      return false;
+    }
+    return !answer.correct; // && this.SelectedAnswers.includes(answer.id);
+  }
+}
--- libs/solid/quiz/src/lib/solid-quiz.module.ts
+++ libs/solid/quiz/src/lib/solid-quiz.module.ts
@@ -3,6 +3,7 @@ import { Route, RouterModule } from '@angular/router';
 import { StartComponent } from './components/start/start.component';
 import { MainComponent } from './components/main/main.component';
 import { QuestionComponent } from './components/question/question.component';
+import { SingleChoiceQuestionComponent } from './components/single-choice-question/single-choice-question.component';
 import { EndComponent } from './components/end/end.component';
 import { NgxsModule } from '@ngxs/store';
 import { QuizState } from './state/quiz.state';
@@ -39,6 +40,7 @@ export const ngxsFeatureModule = NgxsModule.forFeature([QuizState]);
     MainComponent,
     QuestionComponent,
     EndComponent,
+    SingleChoiceQuestionComponent,
   ],
   imports: [
     SolidCoreModule,
