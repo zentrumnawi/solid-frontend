f98df9f3885b1c2d4f2d10d8e7024cbd95d033b0
jdrandomperson <33594579+lthuong@users.noreply.github.com>
2022-05-21 23:24:12 +0200

    Ranking answers filter

--- libs/solid/quiz/src/lib/components/ranking-question/ranking-question.component.ts
+++ libs/solid/quiz/src/lib/components/ranking-question/ranking-question.component.ts
@@ -19,12 +19,14 @@ export class RankingQuestionComponent implements OnInit, OnChanges {
   @Input() public question!: QuizQuestion;
   @Output() public nextQuestionClicked = new EventEmitter<boolean>();
 
-  public SelectedAnswers!: number[];
-  public ShowAnswers!: boolean;
-  public Correct = false;
+  public selectedAnswers!: number[];
+  public showAnswers!: boolean;
+  public correct = false;
+  public index!: number;
 
   public answersList: any[] = [];
 
+  public hasSubsequence = false;
   public subsequence: number[] = [];
   public subsequences: any[] = [];
 
@@ -39,71 +41,76 @@ export class RankingQuestionComponent implements OnInit, OnChanges {
 
   ngOnChanges(changes: SimpleChanges): void {
     if (changes.question.previousValue !== changes.question.currentValue) {
-      this.ShowAnswers = false;
-      this.SelectedAnswers = [];
-      this.Correct = false;
+      this.showAnswers = false;
+      this.selectedAnswers = [];
+      this.correct = false;
     }
   }
 
   drop(event: CdkDragDrop<any[]>) {
     moveItemInArray(this.answersList, event.previousIndex, event.currentIndex);
-    console.log(this.question.answers);
   }
 
   public onShowAnswersClick() {
-    this.ShowAnswers = true;
-    let hasSubsequence = false;
-    for (let i = 0; i < this.question.answers.length; ++i) {
-      if (this.question.answers[i].ranking_position == 1) {
-        hasSubsequence = this.question.answers[i].subsequences;
-        break;
+    this.showAnswers = true;
+
+    this.index = this.question.answers.findIndex((res) => {
+      return res.ranking_position == 1;
+    });
+    this.hasSubsequence = this.question.answers[this.index].subsequences;
+
+    // find all the subsequences
+    for (let i = 0; i < this.answersList.length - 1; ++i) {
+      if (
+        this.answersList[i].correct_position ==
+        this.answersList[i + 1].correct_position - 1
+      ) {
+        if (this.subsequence.length > 1) {
+          this.subsequence.push(this.answersList[i + 1].correct_position);
+        } else {
+          this.subsequence.push(this.answersList[i].correct_position);
+          this.subsequence.push(this.answersList[i + 1].correct_position);
+        }
+      } else {
+        if (this.subsequence.length > 0)
+          this.subsequences.push(this.subsequence);
+        this.subsequence = [];
       }
     }
+    if (this.subsequence.length > 0) this.subsequences.push(this.subsequence);
 
-    // set to hasSubsequence for testing
-    if (hasSubsequence) {
-      this.Correct = true;
-      for (let i = 0; i < this.answersList.length; ++i) {
-        if (this.answersList[i].correct_position != i + 1) {
-          this.Correct = false;
-        }
-      }
-    } else {
-      // find all the subsequences
-      for (let i = 0; i < this.answersList.length - 1; ++i) {
-        if (
-          this.answersList[i].correct_position ==
-          this.answersList[i + 1].correct_position - 1
-        ) {
-          if (this.subsequence.length > 1) {
-            this.subsequence.push(this.answersList[i + 1].correct_position);
-          } else {
-            this.subsequence.push(this.answersList[i].correct_position);
-            this.subsequence.push(this.answersList[i + 1].correct_position);
-          }
-        } else {
-          if (this.subsequence.length > 0)
-            this.subsequences.push(this.subsequence);
-          this.subsequence = [];
-        }
-      }
-      if (this.subsequence.length > 0) this.subsequences.push(this.subsequence);
-
+    if (this.subsequences.length == 0) this.hasSubsequence = false;
+    else if (this.subsequence.length == this.answersList.length)
+      this.correct = true;
+    else {
       // find the longest subsequence
       let max_length = 0;
       let indexOfMax = 0;
       for (let i = 0; i < this.subsequences.length; ++i) {
-        if (this.subsequences[i].length >= max_length) {
+        if (this.subsequences[i].length > max_length) {
           max_length = this.subsequences[i].length;
           indexOfMax = i;
         }
       }
-
-      console.log('Longest subsequence: ' + this.subsequences[indexOfMax]);
+      this.subsequence = this.subsequences[indexOfMax];
     }
   }
 
+  public isCorrectPosition(answer: any) {
+    if (!this.showAnswers) {
+      return false;
+    }
+    return this.answersList.indexOf(answer) + 1 == answer.correct_position;
+  }
+
+  public isInCorrectPosition(answer: any) {
+    if (!this.showAnswers) {
+      return false;
+    }
+    return this.answersList.indexOf(answer) + 1 != answer.correct_position;
+  }
+
   public onNextQuestionClick() {
-    this.nextQuestionClicked.emit(this.Correct);
+    this.nextQuestionClicked.emit(this.correct);
   }
 }
