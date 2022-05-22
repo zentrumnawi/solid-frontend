921a62db6e63125a054b3e9df83856ed3856c73d
jdrandomperson <33594579+lthuong@users.noreply.github.com>
2022-05-22 21:52:52 +0200

    Range component (empty)

---
+++ libs/solid/quiz/src/lib/components/range-question/range-question.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'solid-quiz-range-question',
+  templateUrl: './range-question.component.html',
+  styleUrls: ['./range-question.component.scss'],
+})
+export class RangeQuestionComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
