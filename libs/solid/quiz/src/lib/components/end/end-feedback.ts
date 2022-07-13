9c15ee2fe0441d316737cd6a183d0e8c0a77353d
Bertram Bühner <github@sheepyhollow.de>
2022-07-13 23:21:31 +0200

    formatting newly handled components

--- libs/solid/quiz/src/lib/components/end/end-feedback.ts
+++ libs/solid/quiz/src/lib/components/end/end-feedback.ts
@@ -2,36 +2,36 @@ export const QuizFeedback = {
   e0: [
     'Das war zum Aufwärmen - die nächste Runde wird bestimmt besser!',
     'Kein Grund zur Frustration - einfach durchatmen und die nächste Runde starten.',
-    'Noch ein Kaffee und eine neue Chance?'
+    'Noch ein Kaffee und eine neue Chance?',
   ],
   lt25: [
     '{{correctPercentage}}% richtig - da ist noch etwas Luft nach oben...',
     'Einfach noch eine Runde starten - da geht bestimmt noch was!',
-    'Beim nächsten Mal sind bestimmt mehr Antworten richtig!'
+    'Beim nächsten Mal sind bestimmt mehr Antworten richtig!',
   ],
   lt50: [
     '{{correctPercentage}}% - noch nicht die Hälfte, aber da war schon Schönes dabei.',
     'Vielleicht braucht es noch ein bisschen Übung?',
-    'In der nächsten Runde werden es bestimmt noch mehr richtige Antworten!'
+    'In der nächsten Runde werden es bestimmt noch mehr richtige Antworten!',
   ],
   lt75: [
     '{{correctPercentage}}% ist ein ganz gutes Ergebnis! Geht da noch mehr?',
     'Das klappt ja ganz gut, aber ein bisschen mehr wird es beim nächsten Mal bestimmt!',
-    'Die Hälfte war mindestens richtig! Eine Runde geht bestimmt noch.'
+    'Die Hälfte war mindestens richtig! Eine Runde geht bestimmt noch.',
   ],
   ge75: [
     '{{correctPercentage}}%! Das ist ein prima Ergebnis.',
     'Beim nächsten Mal werden bestimmt die 100% geknackt!',
-    'Nicht schlecht - ein paar % fehlen noch zum Gipfel!'
+    'Nicht schlecht - ein paar % fehlen noch zum Gipfel!',
   ],
   e100: [
     'Alle Fragen richtig!? Beeindruckend...',
     'Na, da kann man wohl nicht mehr viel beibringen.',
-    'Na? Klappt es mit den 100% auch bei der nächsten Runde?'
+    'Na? Klappt es mit den 100% auch bei der nächsten Runde?',
   ],
   nan: [
     'Rock bottom, I hope for your sake you nuked that level!',
     'Wenn gar keine Frage beantwortet wurde, gibt es auch keine Gummipunkte!',
-    'Ein verweigerter Selbsttest? Come on!'
-  ]
+    'Ein verweigerter Selbsttest? Come on!',
+  ],
 };
