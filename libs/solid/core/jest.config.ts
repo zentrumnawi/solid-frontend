2cc65f6b26ce035ce3d469f8afc39c388e1c33cc
lthuong <33594579+lthuong@users.noreply.github.com>
2022-08-27 12:14:17 +0200

    Update Angular - 1st Attempt

---
+++ libs/solid/core/jest.config.ts
@@ -0,0 +1,23 @@
+/* eslint-disable */
+export default {
+  preset: '../../../jest.preset.js',
+  coverageDirectory: '../../../coverage/libs/solid/core',
+
+  setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
+  globals: {
+    'ts-jest': {
+      tsconfig: '<rootDir>/tsconfig.spec.json',
+      stringifyContentPathRegex: '\\.(html|svg)$',
+    },
+  },
+  displayName: 'solid-core',
+  snapshotSerializers: [
+    'jest-preset-angular/build/serializers/no-ng-attributes',
+    'jest-preset-angular/build/serializers/ng-snapshot',
+    'jest-preset-angular/build/serializers/html-comment',
+  ],
+  transform: {
+    '^.+.(ts|mjs|js|html)$': 'jest-preset-angular',
+  },
+  transformIgnorePatterns: ['node_modules/(?!.*.mjs$)'],
+};
