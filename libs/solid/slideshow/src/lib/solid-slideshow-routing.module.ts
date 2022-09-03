54ec1ebdf7f63cf679f125636d661c90d913c334
lthuong <33594579+lthuong@users.noreply.github.com>
2022-09-03 16:18:37 +0200

    Fix slideshow routing issue

--- libs/solid/slideshow/src/lib/solid-slideshow-routing.module.ts
+++ libs/solid/slideshow/src/lib/solid-slideshow-routing.module.ts
@@ -1,5 +1,6 @@
 import { RouterModule, Routes } from '@angular/router';
 import { SlideshowBaseComponent } from './components/slideshow-base/slideshow-base.component';
+import { SlideshowSelectComponent } from './components/slideshow-select/slideshow-select.component';
 
 const routes: Routes = [
   {
@@ -8,13 +9,14 @@ const routes: Routes = [
     children: [
       {
         path: ':categoriesSlug/:slideshowId/:slideshowPageId',
+        component: SlideshowSelectComponent
       },
       {
         path: '**',
-        redirectTo: '',
-      },
-    ],
-  },
+        redirectTo: ''
+      }
+    ]
+  }
 ];
 
 export const SolidSlideshowRoutingModule = RouterModule.forChild(routes);
