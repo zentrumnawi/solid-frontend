2cc5c0a41b17c2be428450da52f47f0a1c16e500
jdrandomperson <33594579+lthuong@users.noreply.github.com>
2022-10-02 10:47:06 +0200

    Revert "Fix slideshow routing issue" -commit issue
    This reverts commit 98f5e958867f9d39141964f97fa62b28d8d1b268.

--- libs/solid/slideshow/src/lib/solid-slideshow-routing.module.ts
+++ libs/solid/slideshow/src/lib/solid-slideshow-routing.module.ts
@@ -1,6 +1,5 @@
 import { RouterModule, Routes } from '@angular/router';
 import { SlideshowBaseComponent } from './components/slideshow-base/slideshow-base.component';
-import { SlideshowSelectComponent } from './components/slideshow-select/slideshow-select.component';
 
 const routes: Routes = [
   {
@@ -9,7 +8,6 @@ const routes: Routes = [
     children: [
       {
         path: ':categoriesSlug/:slideshowId/:slideshowPageId',
-        component: SlideshowSelectComponent,
       },
       {
         path: '**',
