1c9861d3e071ee7c7141da77a281d314ce3a4e98
jdrandomperson <33594579+lthuong@users.noreply.github.com>
2022-10-02 21:43:01 +0200

    Test formatting - (run yarn nx formats)

--- libs/solid/skeleton/src/lib/components/feedback/feedback.component.ts
+++ libs/solid/skeleton/src/lib/components/feedback/feedback.component.ts
@@ -1,5 +1,9 @@
 import { Component, Inject, OnDestroy, OnInit } from '@angular/core';
-import { UntypedFormBuilder, UntypedFormGroup, Validators } from '@angular/forms';
+import {
+  UntypedFormBuilder,
+  UntypedFormGroup,
+  Validators,
+} from '@angular/forms';
 import {
   MAT_DIALOG_DATA,
   MatDialogRef,
