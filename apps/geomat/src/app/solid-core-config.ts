92d72f982308135b0885f0b9b537e58587ed684b
jdrandomperson <33594579+lthuong@users.noreply.github.com>
2023-08-22 22:19:14 +0200

    Profile: Add expandAllgemein to core-cofig

--- apps/geomat/src/app/solid-core-config.ts
+++ apps/geomat/src/app/solid-core-config.ts
@@ -20,4 +20,5 @@ export const coreConfig: SolidCoreConfig = {
   feedbackLogo: 'assets/svg/feedback.svg',
   landingBannerContent: landingBannerContent,
   expandProfileTree: false,
+  expandAllgemein: false,
 };
