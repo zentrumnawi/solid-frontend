6edb14a52b22cf3088e363ffc6f7c62a92c6e279
unknown <imtiazaiub1@gmail.com>
2025-01-13 23:48:02 +0100

    Fix: change in test cases

--- libs/solid/skeleton/schematics/ng-add/index.spec.ts
+++ libs/solid/skeleton/schematics/ng-add/index.spec.ts
@@ -34,106 +34,41 @@ describe('ng-add', () => {
 
     let appTree: UnitTestTree;
     beforeEach(async () => {
-      try {
-        appTree = await testRunner
-          //.runExternalSchematicAsync(
-          .runExternalSchematic(
-            '@schematics/angular',
-            'workspace',
-            workspaceOptions
-          );
-        //.toPromise();
-        appTree = await testRunner
-          //.runExternalSchematicAsync(
-          .runExternalSchematic(
-            '@schematics/angular',
-            'application',
-            appOptions,
-            appTree
-          );
-        //  .toPromise();
-        appTree = await testRunner
-          // .runSchematicAsync(
-          .runSchematic(
-            'ng-add',
-            {
-              name: 'test',
-            },
-            appTree
-          );
-        //.toPromise();
-      } catch (error) {
-        console.error('Error during schematic execution:', error);
-      }
+      appTree = await testRunner.runExternalSchematic(
+        '@schematics/angular',
+        'workspace',
+        workspaceOptions
+      );
+      appTree = await testRunner.runExternalSchematic(
+        '@schematics/angular',
+        'application',
+        appOptions,
+        appTree
+      );
+      appTree = await testRunner.runSchematic(
+        'ng-add',
+        {
+          name: 'test',
+        },
+        appTree
+      );
     });
-    // beforeEach(async () => {
-    //   appTree = (await testRunner.runExternalSchematic(
-    //     '@schematics/angular',
-    //     'workspace',
-    //     workspaceOptions
-    //   ))!;
-
-    //   appTree = (await testRunner.runExternalSchematic(
-    //     '@schematics/angular',
-    //     'application',
-    //     appOptions,
-    //     appTree
-    //   ))!;
-
-    //   appTree = (await testRunner.runSchematic(
-    //     'ng-add',
-    //     { name: 'test' },
-    //     appTree
-    //   ))!;
-    //   expect(appTree.files.length).toBeGreaterThan(0); // Ensure files exist
-    //   console.log('Generated Files:', appTree.files); // Log the files for debugging
-    // });
     it('files created', async () => {
       expect(appTree.files).toEqual(
         expect.arrayContaining([
-          // '/projects/bar/src/styles.scss',
-          // '/projects/bar/src/theme.scss',
-          // '/projects/bar/src/app/solid-core-config.ts',
-          // '/projects/bar/src/app/solid-skeleton-config.ts',
-          // '/projects/bar/src/app/components/landing-banner-content/landing-banner-content.component.ts',
-          // '/projects/bar/src/app/components/landing-banner-content/landing-banner-content.component.scss',
-          // '/projects/bar/src/app/components/landing-banner-content/landing-banner-content.component.html',
-          // '/projects/bar/src/app/components/privacy/privacy.component.ts',
-          // '/projects/bar/src/app/components/privacy/privacy.component.scss',
-          // '/projects/bar/src/app/components/privacy/privacy.component.html',
-
-          '/angular.json',
-          '/package.json',
-          '/README.md',
-          '/tsconfig.json',
-          '/.editorconfig',
-          '/.gitignore',
-          '/.vscode/extensions.json',
-          '/.vscode/launch.json',
-          '/.vscode/tasks.json',
-          '/projects/bar/tsconfig.app.json',
-          '/projects/bar/tsconfig.spec.json',
-          '/projects/bar/src/main.ts',
-          '/projects/bar/src/favicon.ico',
-          '/projects/bar/src/index.html',
           '/projects/bar/src/styles.scss',
-          '/projects/bar/src/app/app.module.ts',
-          '/projects/bar/src/app/app.component.html',
-          '/projects/bar/src/app/app.component.spec.ts',
-          '/projects/bar/src/app/app.component.ts',
-          '/projects/bar/src/app/app.component.scss',
-          '/projects/bar/src/assets/.gitkeep',
+          '/projects/bar/src/theme.scss',
+          '/projects/bar/src/app/solid-core-config.ts',
+          '/projects/bar/src/app/solid-skeleton-config.ts',
+          '/projects/bar/src/app/components/landing-banner-content/landing-banner-content.component.ts',
+          '/projects/bar/src/app/components/landing-banner-content/landing-banner-content.component.scss',
+          '/projects/bar/src/app/components/landing-banner-content/landing-banner-content.component.html',
+          '/projects/bar/src/app/components/privacy/privacy.component.ts',
+          '/projects/bar/src/app/components/privacy/privacy.component.scss',
+          '/projects/bar/src/app/components/privacy/privacy.component.html',
         ])
       );
     });
-
-    // it('dependencies added to package.json', async () => {
-    //   const packageJson = JSON.parse(getFileContent(appTree, '/package.json'));
-    //   const dependencies = packageJson.dependencies;
-    //   DEPENDENCIES.forEach((dep) => {
-    //     expect(dependencies[dep.name]).toEqual(dep.version);
-    //   });
-    // });
 
     it('dependencies added to package.json', async () => {
       const filePath = '/package.json';
@@ -154,14 +89,6 @@ describe('ng-add', () => {
       });
     });
 
-    // it('app module correct', async () => {
-    //   const fileContent = getFileContent(
-    //     appTree,
-    //     '/projects/bar/src/app/app.module.ts'
-    //   );
-
-    //   // expect(fileContent).toContain('declarations: [w AppComponent w]');
-    // });
     it('app module correct', async () => {
       const filePath = '/projects/bar/src/app/app.module.ts';
       if (!appTree.exists(filePath)) {
