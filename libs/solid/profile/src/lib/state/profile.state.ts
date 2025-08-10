cc097d216da7aece7e3ddd7a8b2127e84d09d931
imtiaz2564 <imtiazaiub1@gmail.com>
2025-08-10 13:04:52 +0200

    fix: profile.state.ts

--- libs/solid/profile/src/lib/state/profile.state.ts
+++ libs/solid/profile/src/lib/state/profile.state.ts
@@ -163,7 +163,6 @@ export class ProfileState {
                 .map((profiles: any) => {
                   return profiles[1].map((profile: any) => {
                     const profileName = profile.general_information?.name;
-                    console.log(profileName);
                     const profileSubName =
                       profile.general_information?.sub_name;
                     return {
